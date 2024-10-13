a2c0f96823e997c686c2bf2d59bffa6cd77ad929
njvfi <kutnichtom@gmail.com>
2024-10-13 21:54:27 +0300

    Add project files.

---
+++ Офіс/Models/Videos.cs
@@ -0,0 +1,10 @@
+﻿namespace Офіс.Models
+{
+    public class Videos
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
