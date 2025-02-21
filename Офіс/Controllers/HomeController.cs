02148d2e0da19690b7ad14e9288b6be50c86e5e4
njvfi <kutnichtom@gmail.com>
2025-02-21 12:50:37 +0200

    Created structure of project

--- Офіс/Controllers/HomeController.cs
+++ Офіс/Controllers/HomeController.cs
@@ -11,6 +11,12 @@ namespace Офіс.Controllers
 {
     public class HomeController : Controller
     {
+        //TODO Main page +
+        //TODO About us +
+        //TODO Photo +
+        //TODO Where to watch? +
+        //TODO Contacts +
+
         private readonly ILogger<HomeController> _logger;
 
         private readonly IWebHostEnvironment _webHostEnvironment;
@@ -26,6 +32,11 @@ namespace Офіс.Controllers
         #region Navigation & Constructors 
         public IActionResult Index()
         {
+            return View();
+        }
+
+       /* public IActionResult Events()
+        {
             List<Events> events = _eventsRepository.GetAllEvents();
             EventListViewModel model = new EventListViewModel
             {
@@ -37,63 +48,75 @@ namespace Офіс.Controllers
         public IActionResult Videos()
         {
             return View();
-        }
+        }*/
 
         public IActionResult About_Us()
         {
             return View();
         }
 
-        public IActionResult Event(int id)
+        /*public IActionResult Event(int id)
         {
             if (id == 0) 
             return RedirectToAction("Index");
             Events events = _eventsRepository.GetEvent(id);
             return View(events);
-        }
+        }*/
 
         public IActionResult Create()
         {
             return View(new CreateViewModel());
         }
-        
+
+        public IActionResult Photos()
+        {
+            return View();
+        }
+        public IActionResult Where_to_watch()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+        #endregion
         [HttpPost]
         public async Task<IActionResult> Create(CreateViewModel model)
         {
-			if (model.Image == null || model.Image.Length <= 0)
-			{
-				return View(model);
-			}
-			Events events = new();
+            if (model.Image == null || model.Image.Length <= 0)
+            {
+                return View(model);
+            }
+            Events events = new();
             events.Name = model.Name;
             events.Description = model.Description;
             events.Place = model.Place;
             events.DateTime = model.DateTime;
             events.Cost = model.Cost;
             events.RegistrationLink = model.RegistrationLink;
-			var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","EventPics");
+            var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "EventPics");
 
-			// Проверка существования папки
-			if (!Directory.Exists(uploadDir))
-			{
-				Directory.CreateDirectory(uploadDir); // Создание папки, если она не существует
-			}
+            // Проверка существования папки
+            if (!Directory.Exists(uploadDir))
+            {
+                Directory.CreateDirectory(uploadDir); // Создание папки, если она не существует
+            }
 
-			var filePath = Path.Combine(uploadDir, model.Image.FileName);
+            var filePath = Path.Combine(uploadDir, model.Image.FileName);
 
-			using (var stream = new FileStream(filePath, FileMode.Create))
-			{
-				await model.Image.CopyToAsync(stream);
-			}
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await model.Image.CopyToAsync(stream);
+            }
 
-			//model.Image.Save($"EventPics/{model.Name}.jpg");
-			events.ImageName = model.Image.FileName;
+            //model.Image.Save($"EventPics/{model.Name}.jpg");
+            events.ImageName = model.Image.FileName;
             _eventsRepository.CreateEvent(events);
             return RedirectToAction("Index");
         }
-		#endregion
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
